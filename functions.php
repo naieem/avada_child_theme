cbf295ac85b28002d401d21d417169c1cf7d0c8c
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-07-23 02:49:09 +0600

    shortcode for testimonial added and style updated

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 add_shortcode('homepagebox', 'homepageboxfunc');
+add_shortcode('newsletterbox', 'testimonial_shortcode');
 
 function theme_enqueue_styles() {
 
@@ -48,4 +49,91 @@ echo '<a href="' . $boxes['link'] . '">' . $boxes['linktext'] . '</a>';
 	<?php
 $all_film = ob_get_clean();
 	return $all_film;
+}
+
+function testimonial_shortcode() {
+	ob_start();
+	?>
+	<div class="testimonials_container">
+
+		<div id="newsletter">
+
+			<div class="title special_font">
+
+				Newsletter Signup
+
+			</div>
+
+			<form action="" method="post" id="newsletter_form">
+
+				<input type="hidden" name="wp3_newsletter_subscribe" value="true">
+
+				<input name="wp3newsletter_email" onfocus="if(this.value=='Email Address')this.value='';" onblur="if(this.value=='')this.value='Email Address';" type="text" class="input" value="Email Address">
+
+				<input name="submit" type="submit" class="button" value="Submit">
+
+			</form>
+
+		</div>
+
+		<script type="text/javascript">
+
+
+
+		</script>
+
+		<div class="testimonial_home">
+
+			<h3>
+
+				<a href="http://JRRosenStudio.com/testimonials/">Testimonials</a>
+
+			</h3>
+
+			<script type="text/javascript">
+
+				jQuery(function(){
+
+					jQuery('.testimonial_home .text').cycle();
+
+				});
+
+			</script>
+
+			<div class="text">
+				<div>
+					"On behalf of everyone at the Perkins School for the Blnid, thank you! Your parcipitation in the Thomas and Bessie Horticulture Center..."<br>
+					<br>
+					To read Sandy Sharpio's Testimonial, click below!
+					<br>
+					<em><strong><a href="http://JRRosenStudio.com/perkins-schoo/">Perkins School for the Blind</a></strong></em>
+				</div>
+
+				<div >
+
+					"Thank you so much for rushing the envelopes for me, I greatly appreciate it! They came out beautiful, the bride will be thrilled!..."<br>
+					<br>
+					To read Michelle Choy's full testimonial, click below!
+					<br>
+
+					<em><strong><a href="http://JRRosenStudio.com/758-2/">Michelle Choy</a></strong></em>
+				</div>
+
+				<div>
+					"Just a quick note to thank you for the beautiful work you did for our wedding. The programs, seating scroll, and table cards looked... "<br>
+					<br>
+					To read Amy Logan Maloney's full testimonial, click below!
+					<br>
+					<em><strong><a href="http://JRRosenStudio.com/amy-logan-maloney/">Amy Logan Maloney</a></strong></em>
+				</div>
+			</div>
+
+		</div>
+
+		<!--end testimonial-->
+
+	</div>
+	<?php
+$newsletter_form = ob_get_clean();
+	return $newsletter_form;
 }
