2fd8ad6673fd3cb16b0e11be395d21f942da64b1
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-07-24 02:29:07 +0600

    testimonial homepage shortcode,testimonial list shortcode and testimonial custom post type added

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-add_shortcode('homepagebox', 'homepageboxfunc');
-add_shortcode('newsletterbox', 'testimonial_shortcode');
-
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles'); // querying styles
+add_shortcode('homepagebox', 'homepageboxfunc'); // homepage 3 boxes shortcode
+add_shortcode('newsletterbox', 'testimonial_shortcode'); // testimonial homepage shortcode
+add_shortcode('testimoniallist', 'testimonial_list'); // testimonial list shortcode
+add_action('init', 'testimonial_post_type_init'); // adding testimonial post type
 function theme_enqueue_styles() {
 
 	$parent_style = 'unite-parent-style';
@@ -52,6 +53,22 @@ $all_film = ob_get_clean();
 }
 
 function testimonial_shortcode() {
+	$args = array(
+		'offset' => 0,
+		'category' => 0,
+		'orderby' => 'post_date',
+		'order' => 'DESC',
+		'include' => '',
+		'exclude' => '',
+		'meta_key' => '',
+		'meta_value' => '',
+		'post_type' => 'testimonial',
+		'post_status' => 'draft, publish, future, pending, private',
+		'suppress_filters' => true,
+	);
+
+	$testimonials = wp_get_recent_posts($args, ARRAY_A);
+	wp_reset_query();
 	ob_start();
 	?>
 	<div class="testimonials_container">
@@ -73,59 +90,29 @@ function testimonial_shortcode() {
 				<input name="submit" type="submit" class="button" value="Submit">
 
 			</form>
-
 		</div>
 
-		<script type="text/javascript">
-
-
-
-		</script>
-
 		<div class="testimonial_home">
-
 			<h3>
-
 				<a href="http://JRRosenStudio.com/testimonials/">Testimonials</a>
-
 			</h3>
-
 			<script type="text/javascript">
-
 				jQuery(function(){
-
 					jQuery('.testimonial_home .text').cycle();
-
 				});
-
 			</script>
 
 			<div class="text">
-				<div>
-					"On behalf of everyone at the Perkins School for the Blnid, thank you! Your parcipitation in the Thomas and Bessie Horticulture Center..."<br>
-					<br>
-					To read Sandy Sharpio's Testimonial, click below!
-					<br>
-					<em><strong><a href="http://JRRosenStudio.com/perkins-schoo/">Perkins School for the Blind</a></strong></em>
-				</div>
-
-				<div >
-
-					"Thank you so much for rushing the envelopes for me, I greatly appreciate it! They came out beautiful, the bride will be thrilled!..."<br>
-					<br>
-					To read Michelle Choy's full testimonial, click below!
-					<br>
-
-					<em><strong><a href="http://JRRosenStudio.com/758-2/">Michelle Choy</a></strong></em>
-				</div>
-
-				<div>
-					"Just a quick note to thank you for the beautiful work you did for our wedding. The programs, seating scroll, and table cards looked... "<br>
-					<br>
-					To read Amy Logan Maloney's full testimonial, click below!
-					<br>
-					<em><strong><a href="http://JRRosenStudio.com/amy-logan-maloney/">Amy Logan Maloney</a></strong></em>
-				</div>
+				<?php
+foreach ($testimonials as $testimonial) {
+		echo '<div>"';
+		echo $testimonial["post_excerpt"];
+		echo '"<br><br>';
+		echo 'To read ' . $testimonial["post_title"] . '\'s Testimonial, click below!<br>';
+		echo '<em><strong><a href="' . get_permalink($testimonial["ID"]) . '">' . $testimonial["post_title"] . '</a></strong></em>';
+		echo '</div>';
+	}
+	?>
 			</div>
 
 		</div>
@@ -136,4 +123,89 @@ function testimonial_shortcode() {
 	<?php
 $newsletter_form = ob_get_clean();
 	return $newsletter_form;
+}
+
+function testimonial_list() {
+	$args = array(
+		'offset' => 0,
+		'category' => 0,
+		'orderby' => 'post_date',
+		'order' => 'DESC',
+		'include' => '',
+		'exclude' => '',
+		'meta_key' => '',
+		'meta_value' => '',
+		'post_type' => 'testimonial',
+		'post_status' => 'draft, publish, future, pending, private',
+		'suppress_filters' => true,
+	);
+
+	$testimonials = wp_get_recent_posts($args, ARRAY_A);
+	wp_reset_query();
+	ob_start();
+	foreach ($testimonials as $testimonial) {
+		?>
+		<div class="testimonial_page_container float-left">
+			<div class="testimonial_wrapper float-left">
+				<div class="testimonial_title float-left">
+					<a href="<?php echo get_permalink($testimonial['ID']); ?>"><?php echo $testimonial["post_title"]; ?>'s Testimonial</a>
+				</div>
+				<div class="testimonial_desc float-left">
+					<?php echo $testimonial["post_excerpt"]; ?>
+				</div>
+				<div class="testimonial_excerpt float-left">
+					<?php echo 'To read ' . $testimonial["post_title"] . '\'s Testimonial, click below!<br>'; ?>
+				</div>
+				<div class="testimonial_footer float-left">
+					<em><strong><?php echo $testimonial["post_title"]; ?></strong></em>|
+					<span><?php echo get_the_date('F, Y', $testimonial['ID']); ?></span>|
+					<a href="<?php echo get_permalink($testimonial['ID']); ?>">read full testimonial »</a>
+				</div>
+			</div>
+		</div>
+	<?php }?>
+	<?php
+$testimonial_list = ob_get_clean();
+	return $testimonial_list;
+}
+
+/**
+ * regiseting testimonial post type
+ * @return testimonial post type
+ */
+function testimonial_post_type_init() {
+	$labels = array(
+		'name' => _x('Testimonials', 'post type general name', 'jrrosen-child'),
+		'singular_name' => _x('Testimonials', 'post type singular name', 'jrrosen-child'),
+		'menu_name' => _x('Testimonials', 'admin menu', 'jrrosen-child'),
+		'name_admin_bar' => _x('Testimonials', 'add new on admin bar', 'jrrosen-child'),
+		'add_new' => _x('Add New', 'book', 'jrrosen-child'),
+		'add_new_item' => __('Add New Testimonials', 'jrrosen-child'),
+		'new_item' => __('New Testimonials', 'jrrosen-child'),
+		'edit_item' => __('Edit Testimonials', 'jrrosen-child'),
+		'view_item' => __('View Testimonials', 'jrrosen-child'),
+		'all_items' => __('All Testimonials', 'jrrosen-child'),
+		'search_items' => __('Search Testimonials', 'jrrosen-child'),
+		'parent_item_colon' => __('Parent Testimonials:', 'jrrosen-child'),
+		'not_found' => __('No Testimonials found.', 'jrrosen-child'),
+		'not_found_in_trash' => __('No Testimonials found in Trash.', 'jrrosen-child'),
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'description' => __('Description.', 'jrrosen-child'),
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'query_var' => true,
+		'rewrite' => array('slug' => 'testimonial'),
+		'capability_type' => 'post',
+		'has_archive' => true,
+		'hierarchical' => false,
+		'menu_position' => null,
+		'supports' => array('title', 'author', 'excerpt'),
+	);
+
+	register_post_type('testimonial', $args);
 }
